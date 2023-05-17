67ec9d74106a51c1d4440a5d60ee35507a0a13f9
joseporiolayats <oriol.ayats@gmail.com>
2023-05-17 11:25:23 +0200

    Docstrings and timeout

--- routes/policies.js
+++ routes/policies.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { clientsPromise, policiesPromise } = require('../data');
+let { clientsPromise, policiesPromise } = require('../data');
 const { authenticateToken } = require('../auth');
 
 const allowedRoles = ['admin'];
 
+/**
+ * Middleware to check if the authenticated user's role is included in the allowed roles.
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {function} next - Express next middleware function.
+ */
 const checkUserRole = (req, res, next) => {
     const user = req.user;
     if (!allowedRoles.includes(user.role)) {
@@ -13,6 +19,15 @@ const checkUserRole = (req, res, next) => {
     next();
 };
 
+/**
+ * Route to get user information by policy id.
+ * @route GET /by_policy/:id
+ * @param {string} id - The id of the policy.
+ * @returns {object} 200 - User data corresponding to the policy id.
+ * @returns {Error} 403 - Access denied.
+ * @returns {Error} 404 - Policy or User not found.
+ * @returns {Error} 500 - Internal server error.
+ */
 router.get('/by_policy/:id', authenticateToken, checkUserRole, async (req, res) => {
     try {
         const id = req.params.id.toLowerCase();
@@ -35,16 +50,29 @@ router.get('/by_policy/:id', authenticateToken, checkUserRole, async (req, res) 
             return res.status(404).send('User not found');
         }
 
-
-
         res.status(200).json(user);
+
+        // Clear data after 30 seconds
+        setTimeout(() => {
+            clientsPromise = null;
+            policiesPromise = null;
+            console.log("Cleared fetched data after 30 seconds");
+        }, 30000);
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal server error');
     }
 });
 
-
+/**
+ * Route to get policies by client name.
+ * @route GET /by_client_name/:name
+ * @param {string} name - The name of the client.
+ * @returns {object} 200 - Policies corresponding to the client name.
+ * @returns {Error} 403 - Access denied.
+ * @returns {Error} 404 - User or Policies not found.
+ * @returns {Error} 500 - Internal server error.
+ */
 router.get('/by_client_name/:name', authenticateToken, checkUserRole, async (req, res) => {
     try {
         const name = req.params.name.toLowerCase();
@@ -60,7 +88,7 @@ router.get('/by_client_name/:name', authenticateToken, checkUserRole, async (req
         }
 
         const matchedPolicies = Object.values(policies).filter(
-            (policy) => policy.clientId.toLowerCase() === user.id
+            (policy) =>policy.clientId.toLowerCase() === user.id
         );
 
         if (matchedPolicies.length === 0) {
@@ -68,11 +96,17 @@ router.get('/by_client_name/:name', authenticateToken, checkUserRole, async (req
         }
 
         res.status(200).json(matchedPolicies);
+
+        // Clear data after 30 seconds
+        setTimeout(() => {
+            clientsPromise = null;
+            policiesPromise = null;
+            console.log("Cleared fetched data after 30 seconds");
+        }, 30000);
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal server error');
     }
 });
-
 
 module.exports = router;
